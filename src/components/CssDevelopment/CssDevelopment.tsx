bf0c191baa2f113a9b1c1215f74d15a74510e7cd
David Manzer <dmanzer2@gmail.com>
2025-04-15 17:17:31 -0700

    starting to add the mixins ebook

--- src/components/CssDevelopment/CssDevelopment.tsx
+++ src/components/CssDevelopment/CssDevelopment.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CssDevelopment.scss';
+import cssEbook from '../../assets/images/css-mixins-cover.png';
 
 const CssDevelopment: React.FC = () => {
   return (
@@ -9,10 +10,10 @@ const CssDevelopment: React.FC = () => {
       <div className="dev-section">
         <div className="container">
           <div className="row align-items-center call-to-action">
-            <div className="col-md-8">
+            <div className="col-12 col-md-8">
               <h3 className="mb-md-0 mb-4">Interested in learning or mastering CSS?</h3>
             </div>
-            <div className="col-md-4 text-center"> 
+            <div className="col-12 col-md-4 text-center"> 
               <a
                 href="http://amzn.to/2FTCMIM"
                 target="_blank"
@@ -21,6 +22,18 @@ const CssDevelopment: React.FC = () => {
                 >
                 Get started in development
               </a>
+            </div>
+          </div>
+          <div className="row align-items-center call-to-action mt-3 mb-5">
+            <div className="col-12 col-md-3">
+            <img
+                src={cssEbook}
+                alt=""
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-12 col-md-9">
+              <h3 className="mb-md-0 mb-4">Checkout my ebook on CSS Mixins.<br /> Learn how to use them and why you should</h3>
             </div>
           </div>
         </div>
